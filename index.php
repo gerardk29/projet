a6f29f0758c0dee629ac579697f76e56f0e8b518
gerardk29 <34568986+gerardk29@users.noreply.github.com>
2017-12-15 15:10:58 +0100

    ajout index et apropos

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projet équipe sous Git</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<main>
+	<section>
+		<h1>Schéma Développeurs Front - Back </h1>
+		<img src="img/iceberg.jpg">
+	</section>
+
+	<section>
+		<h2>Notre équipe</h2>
+		<ul>
+			<li>Valentin</li>
+			<li>Thibaud</li>
+			<li>Peter</li>
+			<li>Gerard</li>
+		</ul>
+	</section>
+
+	<section>
+		<h2><a href="apropos.html">A propos</a></h2>
+	</section>
+
+</main>
+
+<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
